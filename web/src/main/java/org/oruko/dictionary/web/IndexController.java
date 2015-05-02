b61eb35c9d92af264ba56f38b194ab49d367ce55
Dadepo Aderemi <dadepo@gmail.com>
2015-05-02 15:13:36 +0200

    #5 switch templating to handlebars (from freemarker)

--- web/src/main/java/org/oruko/dictionary/web/IndexController.java
+++ web/src/main/java/org/oruko/dictionary/web/IndexController.java
@@ -1,7 +1,5 @@
 package org.oruko.dictionary.web;
 
-import org.oruko.dictionary.model.repository.NameEntryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,22 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    @Autowired
-    NameEntryRepository repository;
-
-    @RequestMapping("/")
-    public String indexPage() {
-        return "index";
-    }
-
     @RequestMapping("/dashboard")
     public String showDashBoard() {
-        return "redirect:http://localhost:9000/dashboardapp";
-    }
-
-    @RequestMapping("/template")
-    public String showTemplate() {
-        return "template";
+        return "redirect:http://localhost/dashboardapp";
     }
 
 }
---
+++ web/src/main/java/org/oruko/dictionary/web/frontend/HomeController.java
@@ -0,0 +1,20 @@
+package org.oruko.dictionary.web.frontend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+/**
+ * @author Dadepo Aderemi.
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String indexPage(Map<String, String> map) {
+        map.put("appName", "Yoruba Dictionary Application");
+        return "website";
+    }
+
+}
