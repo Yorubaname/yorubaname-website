cd3de556185064e170ce8cab0f4f7e1695bfeff2
Dadepo Aderemi <dadepo@gmail.com>
2015-05-06 21:48:02 +0200

    YOR-4 Added pid writer to aid stopping running instance via pid

--- website/src/main/java/org/oruko/dictionary/DictionaryApplication.java
+++ website/src/main/java/org/oruko/dictionary/DictionaryApplication.java
@@ -1,6 +1,7 @@
 package org.oruko.dictionary;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,6 +9,8 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.io.File;
 
 /**
  * @author Dadepo Aderemi.
@@ -22,7 +25,10 @@ public class DictionaryApplication {
      * @param args arguments supplied to the application
      */
     public static void main(String[] args) {
-        SpringApplication.run(DictionaryApplication.class, args);
+        File pid = new File("app.pid");
+        SpringApplication app = new SpringApplication(DictionaryApplication.class);
+        app.addListeners(new ApplicationPidFileWriter(pid));
+        app.run(args);
     }
 
     @Bean
