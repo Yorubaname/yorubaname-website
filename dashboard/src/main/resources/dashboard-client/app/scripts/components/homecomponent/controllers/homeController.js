8b701a56741c8c578c62e86bacbf24a421eb20ae
Dadepo Aderemi <dadepo@gmail.com>
2015-08-14 20:56:50 +0200

    YOR-62 Show the number of suggested names in the dashboard's landing page

--- dashboard/src/main/resources/dashboard-client/app/scripts/components/homecomponent/controllers/homeController.js
+++ dashboard/src/main/resources/dashboard-client/app/scripts/components/homecomponent/controllers/homeController.js
@@ -5,13 +5,30 @@
  * @param $scope angular scope
  * @param profileService the service use to fetch profile information
  */
-var homeController = function($scope, profileService, $cookies) {
+var homeController = function ($scope, profileService, $cookies, nameEntryService) {
 
-    $scope.title = "Dictionary Dashboard";
-    $scope.profile = {
-        username: $cookies.username,
-        role: $cookies.role
-    }
+  $scope.info = {};
+  $scope.title = "Dictionary Dashboard";
+  $scope.profile = {
+    username: $cookies.username,
+    role: $cookies.role
+  }
+
+
+  var getSuggestedNamesCount = function () {
+    var result = nameEntryService.getSuggestedNames();
+    result.success(function(response){
+      $scope.info.suggestedNamesCount = response.length;
+    }).error(function(error) {
+      console.log("Error while getting suggested names", error);
+    });
+  };
+
+
+  (function(){
+    // init functions
+    getSuggestedNamesCount();
+  })();
 };
 
 angular.module('dashboardappApp').controller("homeController", homeController);
