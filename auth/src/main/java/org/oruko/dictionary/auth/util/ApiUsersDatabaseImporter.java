00613c9dc124b6770c5167f2d8eaf3b201c5d6b4
Dadepo Aderemi <dadepo@gmail.com>
2015-10-02 18:50:18 +0200

    initialize the auth table only when running in local

--- auth/src/main/java/org/oruko/dictionary/auth/util/ApiUsersDatabaseImporter.java
+++ auth/src/main/java/org/oruko/dictionary/auth/util/ApiUsersDatabaseImporter.java
@@ -3,52 +3,63 @@ package org.oruko.dictionary.auth.util;
 import org.oruko.dictionary.auth.ApiUser;
 import org.oruko.dictionary.auth.ApiUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
 /**
  * Utility class for populating API user with mock users
- *
+ * <p>
  * Created by Dadepo Aderemi.
  */
 @Component
 public class ApiUsersDatabaseImporter {
+
+    @Value("${app.host}")
+    private String host;
 
     @Autowired
     private ApiUserRepository userRepository;
 
     @PostConstruct
     public void initApiUsers() {
-        // demo admin
-        ApiUser admin = new ApiUser();
-        admin.setEmail("admin@example.com");
-        admin.setPassword("admin");
-        admin.setRoles(ApiUser.ROLE.ADMIN.getRole(),
-                       ApiUser.ROLE.DASHBOARD.getRole(),
-                       ApiUser.ROLE.BASIC.getRole());
-        userRepository.save(admin);
 
-        // dashboard user
-        ApiUser dashboard = new ApiUser();
-        dashboard.setEmail("dashboard@example.com");
-        dashboard.setPassword("dashboard");
-        dashboard.setRoles(ApiUser.ROLE.DASHBOARD.getRole(),
+        /**
+         * Only initialize the database only when in dev
+         */
+        if (host.equalsIgnoreCase("localhost")) {
+
+            // demo admin
+            ApiUser admin = new ApiUser();
+            admin.setEmail("admin@example.com");
+            admin.setPassword("admin");
+            admin.setRoles(ApiUser.ROLE.ADMIN.getRole(),
+                           ApiUser.ROLE.DASHBOARD.getRole(),
                            ApiUser.ROLE.BASIC.getRole());
-        userRepository.save(dashboard);
+            userRepository.save(admin);
 
-        // lexi user
-        ApiUser lexicographer = new ApiUser();
-        lexicographer.setEmail("lexicographer@example.com");
-        lexicographer.setPassword("lexicographer");
-        lexicographer.setRoles(ApiUser.ROLE.LEXICOGRAPHER.getRole());
-        userRepository.save(lexicographer);
+            // dashboard user
+            ApiUser dashboard = new ApiUser();
+            dashboard.setEmail("dashboard@example.com");
+            dashboard.setPassword("dashboard");
+            dashboard.setRoles(ApiUser.ROLE.DASHBOARD.getRole(),
+                               ApiUser.ROLE.BASIC.getRole());
+            userRepository.save(dashboard);
 
-        // normal user
-        ApiUser basic = new ApiUser();
-        basic.setEmail("basic@example.com");
-        basic.setPassword("basic");
-        basic.setRoles(ApiUser.ROLE.BASIC.getRole());
-        userRepository.save(basic);
+            // lexi user
+            ApiUser lexicographer = new ApiUser();
+            lexicographer.setEmail("lexicographer@example.com");
+            lexicographer.setPassword("lexicographer");
+            lexicographer.setRoles(ApiUser.ROLE.LEXICOGRAPHER.getRole());
+            userRepository.save(lexicographer);
+
+            // normal user
+            ApiUser basic = new ApiUser();
+            basic.setEmail("basic@example.com");
+            basic.setPassword("basic");
+            basic.setRoles(ApiUser.ROLE.BASIC.getRole());
+            userRepository.save(basic);
+        }
     }
 }
