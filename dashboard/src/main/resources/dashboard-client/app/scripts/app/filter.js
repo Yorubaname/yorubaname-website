d6d95b907275faedd1d40a242e7078e85b9c1542
Yemi Agbetunsin <yemi.agbetunsin@gmail.com>
2015-10-06 19:34:26 +0100

    copied all filters into one file under app dir

---
+++ dashboard/src/main/resources/dashboard-client/app/scripts/app/filter.js
@@ -0,0 +1,31 @@
+'use strict';
+
+angular.module('dashboardappApp')
+
+/**
+ * Filter to parse delimated string to arrays
+ */
+
+  .filter('sToArrays', function() {
+    return function(text, delimeter) {
+      if (!text || !delimeter) return text;
+      return text.split(delimeter)
+    }
+  })
+
+/**
+ * Filter to parse input value arrays to single inline strings
+ */
+
+  .filter('aToString', function() {
+    return function(inputArray, delimeter) {
+      var val = "";
+      for (var i = inputArray.length - 1; i >= 0; i--) {
+            val += inputArray[i].text
+            if (i != 0) {
+                val += delimeter
+            }
+      }
+      return val
+    }
+  })
