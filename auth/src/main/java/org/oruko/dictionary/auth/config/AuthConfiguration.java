1c8da81f00c3e45f4062b70a4b2106de1fb2b9aa
Dadepo Aderemi <dadepo@gmail.com>
2015-10-19 21:02:11 +0200

    Updated authorization rules

--- auth/src/main/java/org/oruko/dictionary/auth/config/AuthConfiguration.java
+++ auth/src/main/java/org/oruko/dictionary/auth/config/AuthConfiguration.java
@@ -20,17 +20,29 @@ public class AuthConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
+                // dashboard
+            .antMatchers("/dashboardapp/**").permitAll()
+                // suggest auth
             .antMatchers(HttpMethod.POST, "/v1/suggest").permitAll()
+            .antMatchers(HttpMethod.DELETE, "/v1/suggest/*").hasAnyRole("ADMIN", "LEXICOGRAPHER")
+                // feedback auth
             .antMatchers(HttpMethod.POST, "/v1/*/feedback").permitAll()
-            .antMatchers(HttpMethod.DELETE, "/v1/*/feedback").hasAnyRole("ADMIN", "DASHBOARD")
-            .antMatchers(HttpMethod.DELETE, "/v1/suggest/*").hasAnyRole("ADMIN", "DASHBOARD")
+            .antMatchers(HttpMethod.DELETE, "/v1/*/feedback").hasAnyRole("ADMIN")
+                // auth auth
             .antMatchers(HttpMethod.POST, "/v1/auth/login").permitAll()
+                // names endpoint auth
             .antMatchers(HttpMethod.DELETE, "/v1/names/delete").hasRole("ADMIN")
             .antMatchers(HttpMethod.DELETE, "/v1/names/*").hasRole("ADMIN")
-            .antMatchers(HttpMethod.POST, "/v1/**").hasAnyRole("ADMIN", "DASHBOARD")
-            .antMatchers(HttpMethod.PUT, "/v1/**").hasAnyRole("ADMIN", "DASHBOARD")
-            .antMatchers("/dashboardapp/**").permitAll()
-            .antMatchers("/v1/**").permitAll()
+                // search endpoint auth
+            .antMatchers(HttpMethod.POST, "/v1/search/*").hasRole("ADMIN")
+            .antMatchers(HttpMethod.PUT, "/v1/search/*").hasRole("ADMIN")
+                // if none of the pattern above matches then stick to the rule
+                // that only Admin and Lexicographer can post and put
+            .antMatchers(HttpMethod.POST, "/v1/**").hasAnyRole("ADMIN", "LEXICOGRAPHER")
+            .antMatchers(HttpMethod.PUT, "/v1/**").hasAnyRole("ADMIN", "LEXICOGRAPHER")
+                // Well get should be available to all
+            .antMatchers(HttpMethod.GET, "/v1/**").permitAll()
+            .antMatchers("/v1/**").permitAll() //TODO revisit. Might be redundant
             .and()
                 .httpBasic();
     }
