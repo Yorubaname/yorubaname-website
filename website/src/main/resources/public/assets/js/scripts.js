0feb8591028aebf329c6420fcb32d48ea568461d
Yemi Agbetunsin <yemi.agbetunsin@gmail.com>
2015-12-05 18:37:03 +0100

    fixed global bug

--- website/src/main/resources/public/assets/js/scripts.js
+++ website/src/main/resources/public/assets/js/scripts.js
@@ -1,18 +1,18 @@
+var alert_error = function (error) {
+    return '<div class="alert alert-danger alert-dismissible" role="alert">' +
+        '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>' +
+        error +
+    '</div>';
+}
+
+var alert_success = function (message) {
+    return '<div class="alert alert-success alert-dismissible" role="alert">' +
+        '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>' +
+        message +
+    '</div>';
+}
+
 $(document).ready(function () {
-
-	var alert_error = function (error) {
-	    return '<div class="alert alert-danger alert-dismissible" role="alert">' +
-	        '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>' +
-	        error +
-	    '</div>';
-	}
-
-	var alert_success = function (message) {
-	    return '<div class="alert alert-success alert-dismissible" role="alert">' +
-	        '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>' +
-	        message +
-	    '</div>';
-	}
 
 	$("#tts-button").on("click", function() {
 		var host = $("#host").html();
