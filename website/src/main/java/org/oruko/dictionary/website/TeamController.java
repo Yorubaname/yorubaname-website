98b9d13f9cb569de4cd92769cae9f224bfed0d4a
Dadepo Aderemi <dadepo@gmail.com>
2015-12-30 09:16:37 +0100

    Added controller for /team. Updated message bundle a lil bit

---
+++ website/src/main/java/org/oruko/dictionary/website/TeamController.java
@@ -0,0 +1,19 @@
+package org.oruko.dictionary.website;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Controller for Team page
+ */
+
+@Controller
+public class TeamController {
+
+    @RequestMapping("/team")
+    public String aboutUsIndexPage(Model map) {
+        map.addAttribute("title", "Team");
+        return "team";
+    }
+}
