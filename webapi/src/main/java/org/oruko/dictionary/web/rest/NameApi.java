47345650518873ef479c11a6d485254c0b46a546
Mateus Oliveira Malaquias <mateus.malaquias@fcamara.com.br>
2017-10-30 09:21:36 -0200

    Removed generic import to favor of explicitly imports

--- webapi/src/main/java/org/oruko/dictionary/web/rest/NameApi.java
+++ webapi/src/main/java/org/oruko/dictionary/web/rest/NameApi.java
@@ -34,7 +34,14 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.Collections;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.function.Predicate;
